6c2c9bfcfcb7da73c88291b2dd0dab45e3a90daa
robert-hh <robert@hammelrath.com>
2025-06-26 10:12:21 +0200

    ads1256: Initial commit.
    Untested with the final device.
    Imports without errors. Runs the code without errors with a
    dummy device creating ADC data output.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

---
+++ README.md
@@ -0,0 +1,177 @@
+# WARNING: This driver is in a draft state and not yet tested.
+
+It's published for discussion purposes. Tests will probably be done until
+July 4, 2025.
+
+# ADS1256: MicroPython classes for the ADS1256 and ADS1255 ADC
+
+This is a short and simple class for the ADS1256 and ADS1255 ADC. It supports reading
+the ADC value, reading the temperature and configuring the various device
+modes. In the following document, the ADS1256 or ADS1256 is used for both ADS1256 and ADS1255, unless
+otherwise stated.
+
+~~Tested with MicroPython ports for RP2040, STM32, SAMD, i.MX RT (e.g. Teensy),  
+ESP32, ESP8266, NRF52840 and W600. Approximate times for reading an ADC value:~~
+- ~~RP2040 at 125 MHz: 450 µs~~
+- ~~PYBD SF6 at 192 MHz: 250 µs~~
+- ~~Teensy 4.1 at 600 MHz: 100 µs~~
+- ~~SAMD51 at 120 MHz: 450 µs~~
+- ~~SAMD21 at 48 MHz: 1.2 ms~~
+- ~~ESP32 at 160 MHz: 900 µs~~
+- ~~ESP8266 at 80 MHz: 1.2 ms~~
+- ~~NRF52840: 800µs.~~
+- ~~Renesas RA6M2 at 120 MHz: 600µs~~
+- ~~W600 at 80 MHz: 900 µs~~ 
+
+~~The nrf port has the problem that the device cannot be configured when using the IRQ based er.
+NRF52840 based devices have worked in the test
+using the polling driver. Otherwise the ADS1256 operates at it's default
+mode, which is gain=1, rate=1000, channel=0.
+pin.irq() seems not to work at the Renesas port, at least not with the tested EV-RA6M2 board.
+It can be configured, but refuses to work. The polling driver works.~~
+
+
+## Constructor
+
+### ADS1256 = ADS1256(spi, cs, drdy)
+### ADS1255 = ADS1255(spi, cs, drdy)
+
+This is the interface constructor. spi must be a SPI object, cs and drdy are the pin objects
+of the GPIO pins used for the respective signals.
+When calling the constructor, a default channel 0 will be defined with AIN0 and AINCOM as inputs,
+a gain of 1 and a rate of 1000.
+
+
+## Methods
+
+### ads1256.channel(id, ainp, ainn=8, gain=1, rate=1000)
+
+Define ro change a logical channel used to read the data. This channel is not identical to
+the ADC input pins. It defines the configuration of input pins, gain and sampling rate.
+id: A number used to identify the channel. It can be any number.
+ainp: The positive input of the channel. Valid values are 0-7 for ADS1256 and 0-1 for ADS1255 or 8
+for the AINCOM signal.
+ainn: The negative input of the channel. Valid values are 0-7 for ADS1256 and 0-1 for ADS1255 or 8
+for the AINCOM signal.
+gain: The gain set for the channel.
+rate: The sampling rate of the channel.
+
+### ads1256.read(channel, [buffer])
+
+Read and ADC value with the configuration set for the channel. If buffer is supplied,
+it will be filled with data in the read continuous mode until the buffer is filled.
+The buffer must be an array of array.array() type 'i' (4 byte quantities). The return value
+is the number of sampled values.
+If buffer is not supplied, a single read is performed and the value is returned.
+
+If the channel used by read is different from the previous channel, the device
+is reconfigured before reading and re-calibrated, if needed.
+
+### ads1256.calibration(mode)
+
+Request a self-calibration of the device. Argument values:
+
+1: calibrate the gain
+2: calibrate the offset
+3: calibrate gain and offset
+
+Other values are ignored.
+
+### ads1256.standby()
+
+Send the standby command.
+
+### ads1256.wakeup()
+
+Send the wakeup command.
+
+### ads1256.reset()
+
+Send the reset command and calibrate the chip.
+
+### ads1256.write_cmd(cmd)
+
+Send the command cmd to the device.
+
+### ads1256.write_reg(reg, data)
+
+Write data to the register(s) starting at reg. data must be either a single integer or
+an object with buffer protocol.
+
+### ads1256.read_reg(reg, number=1)
+
+Return the content of the register(s) starting at reg. The number of registers
+is provided by number.
+
+### ads1256()
+
+Perform a single read using the previously used channel.
+
+Other methods exist but are preferred for internal use.
+
+## Constants
+
+When constants are to be used outside the class, they must be prefixed
+with the respective class name ADS1256 or ADS1255 or instance name.
+
+### Registers
+
+    REG_STATUS = 0x00
+    REG_MUX = 0x01
+    REG_ADCON = 0x02
+    REG_DRATE = 0x03
+    REG_IO = 0x04
+    REG_OFC0 = 0x05
+    REG_OFC1 = 0x06
+    REG_OFC2 = 0x07
+    REG_FSC0 = 0x08
+    REG_FSC1 = 0x09
+    REG_FSC2 = 0x0A
+
+### Commands
+
+    CMD_WAKEUP = 0x00
+    CMD_RDATA = 0x01
+    CMD_RDATAC = 0x03
+    CMD_SDATAC = 0x0F
+    CMD_RREG = 0x10
+    CMD_WREG = 0x50
+    CMD_SELFCAL = 0xF0
+    CMD_SELFOCAL = 0xF1
+    CMD_SELFGCAL = 0xF2
+    CMD_SYSOCAL = 0xF3
+    CMD_SYSGCAL = 0xF4
+    CMD_SYNC = 0xFC
+    CMD_STANDBY = 0xFD
+    CMD_RESET = 0xFE
+
+### Others
+
+    AINCOM = 8
+    NUM_INPUTS = 8
+    SELFCAL_GAIN = 1
+    SELFCAL_OFFSET = 2
+
+## Example
+
+    # Example for using the driver with a RP2 device
+    # SPI at GPIO12 - GPIO15, DRDY at GPIO11
+    #
+    from machine import SPI, Pin
+    from array import array
+    from ads1256 import ADS1256
+
+    # Set up the interface. By default, channel 0 is created
+    spi = SPI(1, sck=14, mosi=15, miso=12)
+    ads1256 = ADS1256(spi, Pin(13), Pin(11))
+    data  = array("i", bytearray(256*4))
+
+    # read 256 values from the device
+    num_values = ads1256.read(0, data)
+
+    # Create a second channel with differential inputs AIN1 <-> AIN2,
+    # gain=2, rate=100
+    ads1256.channel(2, 1, 2, gain=2, rate=100)
+
+    # Read a single value
+    value = read(2)
---
+++ ads1256.py
@@ -0,0 +1,306 @@
+# MIT License
+
+# Copyright (c) 2025 Robert Hammelrath
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from machine import Pin
+import time
+import array
+
+class ADS1256:
+    # Registers
+    REG_STATUS = const(0x00)
+    REG_MUX = const(0x01)
+    REG_ADCON = const(0x02)
+    REG_DRATE = const(0x03)
+    REG_IO = const(0x04)
+    REG_OFC0 = const(0x05)
+    REG_OFC1 = const(0x06)
+    REG_OFC2 = const(0x07)
+    REG_FSC0 = const(0x08)
+    REG_FSC1 = const(0x09)
+    REG_FSC2 = const(0x0A)
+
+    #Commands
+    CMD_WAKEUP = const(0x00)
+    CMD_RDATA = const(0x01)
+    CMD_RDATAC = const(0x03)
+    CMD_SDATAC = const(0x0F)
+    CMD_RREG = const(0x10)
+    CMD_WREG = const(0x50)
+    CMD_SELFCAL = const(0xF0)
+    CMD_SELFOCAL = const(0xF1)
+    CMD_SELFGCAL = const(0xF2)
+    CMD_SYSOCAL = const(0xF3)
+    CMD_SYSGCAL = const(0xF4)
+    CMD_SYNC = const(0xFC)
+    CMD_STANDBY = const(0xFD)
+    CMD_RESET = const(0xFE)
+
+    _rate = {
+         30000: 0b11110000,
+         15000: 0b11100000,
+         7500: 0b11010000,
+         3750: 0b11000000,
+         2000: 0b10110000,
+         1000: 0b10100001,
+         500: 0b10010010,
+         100: 0b10000010,
+         60: 0b01110010,
+         50: 0b01100011,
+         30: 0b01010011,
+         25: 0b01000011,
+         15: 0b00110011,
+         10: 0b00100011,
+         5: 0b00010011,
+         2.5: 0b00000011,
+    }
+
+    _gain = {
+        1 : 0b0000,
+        2 : 0b0001,
+        4 : 0b0010,
+        8 : 0b0011,
+        16: 0b0100,
+        32: 0b0101,
+        64: 0b0110,
+    }
+
+    AINCOM = const(8)
+    NUM_INPUTS = 8
+    SELFCAL_GAIN = const(1)
+    SELFCAL_OFFSET = const(2)
+
+    def __init__(self, spi, cs, drdy):
+        self.spi = spi
+        self.cs = cs
+        self.drdy = drdy
+        self.cs.init(mode=Pin.OUT, value=1)
+        self.drdy.init(mode=Pin.IN)
+
+        self.buffer_1 = bytearray(1)
+        self.buffer_2 = bytearray(2)
+        self.buffer_3 = bytearray(3)
+ 
+        # The channel table is a list of logical channels with it's
+        # configuration of inputs, gain and rate. If needed,
+        # the device will be reconfigured when the respective
+        # channel is used. A channel definition is created or modified
+        # with the channel() method.
+
+        self.reset()
+        self.channel_table = { }
+        # create a single default entry.
+        self.channel(0, 0, AINCOM, 1, 1000)
+
+    def __call__(self):
+        return self.read(self.previous_channel)
+
+    # Write to the registers. data is either an object with buffer
+    # protocol or for convenience a single number.
+    def write_reg(self, reg, data):
+        if reg > REG_FSC2:
+            raise ValueError("Invalid register")
+        if type(data) is int:
+            self.buffer_3[0] = CMD_WREG + reg
+            self.buffer_3[1] = 0
+            self.buffer_3[2] = data
+            self.cs(0)
+            self.spi.write(self.buffer_3)
+            self.cs(1)
+        else:
+            self.buffer_2[0] = CMD_WREG + reg
+            self.buffer_2[1] = len(data) - 1
+            self.cs(0)
+            self.spi.write(self.buffer_2)
+            self.spi.write(data)
+            self.cs(1)
+
+    # Read number bytes from register reg.
+    def read_reg(self, reg, number=1):
+        if reg > REG_FSC2:
+            raise ValueError("Invalid register")
+        self.buffer_2[0] = CMD_RREG + reg
+        self.buffer_2[1] = number - 1
+        self.cs(0)
+        self.spi.write(self.buffer_2)
+        time.sleep_us(6)
+        result = self.spi.read(number)
+        self.cs(1)
+        return result
+
+    def write_cmd(self, cmd):
+        self.buffer_1[0] = cmd
+        self.cs(0)
+        self.spi.write(self.buffer_1)
+        self.cs(1)
+
+    def _drdy_cb(self, drdy):
+        self.drdy.irq(handler=None)
+        self.__drdy = True
+
+    def _wait_for_drdy(self):
+        # # Set up the trigger for conversion enable.
+        # self.__drdy = False
+        # self.drdy.irq(trigger=Pin.IRQ_FALLING, handler=self._drdy_cb)
+        # # Wait about 1 second for the DRDY event
+        # for _ in range(20000):
+        #     if self.__drdy is True:
+        #         break
+        #     time.sleep_us(50)
+        # else:
+        #     self.__drdy = False
+        #     self.data.irq(handler=None)
+        #     raise OSError("Sensor does not respond")
+        drdy = self.drdy
+        # wait for DRDY being high
+        start = time.ticks_ms()
+        while time.ticks_diff(time.ticks_ms(), start) < 1000:
+            if drdy():
+                break
+        else:
+            raise OSError("No DRDY pulse found")
+        # wait for DRDY getting low
+        for _ in range(20000):
+            if not drdy():
+                break
+            time.sleep_us(50)
+        else:
+            raise OSError("Sensor does not respond")
+
+    # read a single value or a set of values from a channel, which
+    # has to be defined using the channel() method.
+    def read(self, channel, buffer=None):
+        if channel != self.previous_channel:
+            self.channel_setup(channel)
+
+        if buffer is None:
+            self._wait_for_drdy()
+            # send the command and get the data
+            self.buffer_1[0] = CMD_RDATA
+            self.cs(0)
+            self.spi.write(self.buffer_1)
+            time.sleep_us(6)
+            self.spi.readinto(self.buffer_3)
+            self.cs(1)
+            result = self.buffer_3[0] << 16 | self.buffer_3[1] << 8 | self.buffer_3[0]
+            if result > 0x7FFFFF:
+                result -= 0x1000000
+            return result
+        else:    
+            # Send the command and get one data item, which
+            # is discarded
+            self._wait_for_drdy()
+            self.buffer_1[0] = CMD_RDATAC
+            self.cs(0)
+            self.spi.write(self.buffer_1)
+            time.sleep_us(6)
+            self.spi.readinto(self.buffer_3)
+            self.cs(1)
+
+            # now get the bulk of data
+            for i in range(len(buffer)):        
+                self._wait_for_drdy()
+                self.cs(0)
+                self.spi.readinto(self.buffer_3)
+                self.cs(1)
+                result = self.buffer_3[0] << 16 | self.buffer_3[1] << 8 | self.buffer_3[0]
+                if result > 0x7FFFFF:
+                    result -= 0x1000000
+                buffer[i] = result
+            self.buffer_1[0] = CMD_SDATAC
+            self.cs(0)
+            self.spi.write(self.buffer_1)
+            self.cs(1)
+            return len(buffer)
+
+    # configure the channel
+    def channel_setup(self, channel):
+        if channel not in self.channel_table.keys():
+            raise ValueError("channel not defined")
+        # set the mux, gain and rate in one transfer
+        if self.previous_channel is not None:
+            config_old = self.channel_table[previous_channel]
+            config_new = self.channel_table[channel]
+            self.write_reg(REG_MUX, config_new)
+            self.previous_channel = channel
+            # perform calibration if needed
+            cal_mode = (config_new[1] != config_old[1]) | ((config_new[2] != config_old[2]) << 1)
+            self.calibration(cal_mode)
+        # Wait for the actual cycle to finish. May not be needed.
+        self._wait_for_drdy()
+
+    # define or redefine a logical channel
+    def channel(self, channel, ainp, ainn=AINCOM, gain=None, rate=None):
+        # check the argument values
+        if not (ainp == AINCOM or 0 <= ainp < self.NUM_INPUTS):
+            raise ValueError("invalid input ainp")
+        if not (ainn == AINCOM or 0 <= ainn < self.NUM_INPUTS):
+            raise ValueError("invalid input ainn")
+        if not (gain is None or gain in self._gain.keys()):
+            raise ValueError("invalid value for gain")
+        if not (rate is None or rate in self._rate.keys()):
+            raise ValueError("invalid value for rate")
+
+        if channel in self.channel_table.keys():
+            # redefine a channel
+            config = self.channel_table[channel]
+            if gain is not None:
+                config[1] = self._gain[gain]
+            if rate is not None:
+                config[2] = self._rate[rate]
+        else:
+            # define a new channel.
+            config = bytearray(3)
+            config[1] = 1 if gain is None else self._gain[gain]
+            config[2] = 1000 if rate is None else self._rate[rate]
+        # the input numbers are always set and must not be None
+        config[0] = (ainp << 4) | ainn
+        self.channel_table[channel] = config
+
+    def standby(self):
+        self.write_cmd(CMD_STANDBY)
+
+    def wakeup(self):
+        self.write_cmd(CMD_WAKEUP)
+
+    def reset(self):
+        self.write_cmd(CMD_RESET)
+        self.previous_channel = None
+
+    def calibration(self, mode=SELFCAL_GAIN | SELFCAL_OFFSET):
+        if mode == SELFCAL_GAIN | SELFCAL_OFFSET:
+            self.buffer_1[0] = CMD_SELFCAL
+        elif mode == SELFCAL_GAIN:   
+            self.buffer_1[0] = CMD_SELFGCAL
+        elif mode == SELFCAL_OFFSET:   
+            self.buffer_1[0] = CMD_SELFOCAL
+        else:
+            return
+        self.cs(0)
+        self.spi.write(self.buffer_1)
+        self.cs(1)
+        # Wait for the calibration to finish
+        self._wait_for_drdy()
+
+
+class ADS1255(ADS1256):
+
+    NUM_INPUTS = 2
